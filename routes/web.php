4c3c2c31e813c72a37b5d1622df610073dd3960f
younghiro <younghiromu112010@icloud.com>
2022-07-09 19:39:43 +0200

    ログインフォームvalidation

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Auth\AuthController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +12,13 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+//Route::get('/user', [UserController::class, 'index']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [AuthController::class, 'showLogin'])->name('showLogin');
+
+
+Route::post('/', [AuthController::class, 'login'])->name('login');
+
+
+
